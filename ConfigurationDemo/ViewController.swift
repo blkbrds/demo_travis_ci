273e94f2cc6266579526e015bfa4ce6e1a7d0415
Vinh Ho X <vinh.ho@asisantech.vn>
2019-08-02 16:21:03 +0700

    init source

---
+++ ConfigurationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConfigurationDemo
+//
+//  Created by MBA0049 on 8/2/19.
+//  Copyright © 2019 MBA0049. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
